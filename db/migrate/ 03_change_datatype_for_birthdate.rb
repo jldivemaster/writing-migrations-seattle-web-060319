6c4ad241ffa0456e3c86953bd0edfe9e1d3b21e9
Jason Leach <jlspursfan@gmail.com>
2019-06-17 21:06:25 +0000

    Automatically backed up by Learn

--- db/migrate/ 03_change_datatype_for_birthdate.rb
+++ db/migrate/ 03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[4.2]
   def change
-    add_column :students, :birthdate, :datetime
+    change_column(students, birthdate, datetime)
   end
 end
